3ca512cc588dd956944dd1097b70073b827f39d6
Alexandre Valdivino <alexandre.valdivino@macrolabs.com.br>
2023-10-20 15:48:44 -0300

    chore: testing done

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,38 @@
-describe('', () => {
-  test('', () => {
-    expect(2).toBe(2)
+// describe('', () => {
+//   test('', () => {
+//     expect(2).toBe(2)
+//   })
+// })
+
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('No name was provided'))
+  })
+})
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('No email was provided'))
   })
 })
